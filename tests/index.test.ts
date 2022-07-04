42f97311417e774f47e9059481bc2f313c573212
Tom Johnsen <105308316+tom-johnsen@users.noreply.github.com>
2022-07-04 22:30:52 +0200

    Update without good desription

---
+++ tests/index.test.ts
@@ -0,0 +1,7 @@
+describe("Dummy tests", () => {
+    it("should assert that true is true", () => expect(true).toBe(true))
+    it("should assert that false is false", () => expect(false).toBe(false))
+    it("should assert that 1 is 1", () => expect(1).toBe(1))
+    it("should assert that 0 is 0", () => expect(0).toBe(0))
+    it("should assert that -1 is -1", () => expect(-1).toBe(-1))
+})
